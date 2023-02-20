3fc5f1c65cf465ad79c14b116261bf7a4fcfc527
Betim Beja <11160171+BetimBeja@users.noreply.github.com>
2023-02-20 21:29:40 +0100

    fixed uglyworkaround

--- uglyWorkaround.js
+++ uglyWorkaround.js
@@ -2,8 +2,8 @@ const replace = require('replace-in-file');
 
 const options = {
     files: require.resolve('pcf-scripts/webpackConfig.js'),
-    from: "ESBuildMinifyPlugin({ target: 'es2015'})",
+    from: /ESBuildMinifyPlugin\s*\({\s*target:\s*'es2015'\s*}\s*\)/g,
     to: "ESBuildMinifyPlugin({ target: 'es2015', sourcemap: true})",
 };
 
-console.log(replace.sync(options));
+replace.sync(options);
